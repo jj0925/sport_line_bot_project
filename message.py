3d8643808e2493c3ea6349ce0765408a2106905d
jj0925 <125723529+jj0925@users.noreply.github.com>
2024-04-08 20:37:07 +0800

    Update message.py

--- message.py
+++ message.py
@@ -65,7 +65,7 @@ def Carousel_Template_Sport():
                     ]
                 ),
                 CarouselColumn(
-                    thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
+                    thumbnail_image_url='https://i.ibb.co/qnMNHg0/2024-04-05-175234.png',
                     title='這是第二塊模板',
                     text='副標題可以自己改',
                     actions=[
@@ -84,7 +84,7 @@ def Carousel_Template_Sport():
                     ]
                 ),
                 CarouselColumn(
-                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
+                    thumbnail_image_url='https://i.ibb.co/qnMNHg0/2024-04-05-175234.png',
                     title='這是第三個模塊',
                     text='最多可以放十個',
                     actions=[
