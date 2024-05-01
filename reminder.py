67f471c4e22e9d1d51d781598f09c8ff3c6844dd
jj0925 <125723529+jj0925@users.noreply.github.com>
2024-05-01 19:19:25 +0800

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -3,7 +3,7 @@ import time
 
 def check_reminders():
     while True:
-        now = datetime.now()..strftime('%H:%M')
+        now = datetime.now().strftime('%H:%M')
         for user_id, reminder_time in reminders.items():
             if now == reminder_time:
                 line_bot_api.push_message(user_id, TextSendMessage(text="該睡覺了！"))
