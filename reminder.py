b60d264817a1ad7db1baf52a883e75f0631e3e3d
jj0925 <125723529+jj0925@users.noreply.github.com>
2024-05-02 11:44:50 +0800

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import pytz
 import time
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
@@ -6,7 +7,8 @@ from linebot.models import *
 
 def check_reminders(reminders):
     while True:
-        now = datetime.now().strftime('%H:%M')
+        taipei = pytz.timezone('Asia/Taipei')
+        now = datetime.now(taipei).strftime('%H:%M')
         for reminder_user_id, reminder_time in reminders.items():
             if now == reminder_time:
                 line_bot_api.push_message(reminder_user_id, TextSendMessage(text="該睡覺了！"))
