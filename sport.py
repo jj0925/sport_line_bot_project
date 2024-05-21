4c4bfb8346305fae8cf32733fed92b49c287d1e2
jj0925 <125723529+jj0925@users.noreply.github.com>
2024-05-21 13:23:38 +0800

    Create sport.py

---
+++ sport.py
@@ -0,0 +1,112 @@
+#上肢
+def upper_limbs():
+  link = ["https://www.youtube.com/watch?v=igh33_IHbrs&pp=ygUM5LiK6IKi6KiT57e0"
+        ,"https://www.youtube.com/watch?v=z7cCerq9OOQ"
+       ]
+  return link
+#胸肌
+def pecs():
+    links = [
+        "https://www.youtube.com/watch?v=Iiwaj6ECX7Q&pp=ygUY6IO46YOo6IOM6YOo5o",
+        "https://www.youtube.com/watch?v=VMvsHq92nlw&pp=ygUM6IO46IKM6KiT57e0",
+        "https://www.youtube.com/watch?v=qVf2QmFNV0o&pp=ygUM6IO46IKM6KiT57e0"
+    ]
+    return links
+#手臂
+def arm():
+    links = [
+        "https://www.youtube.com/watch?v=ZwJqHlg6230&pp=ygUM5omL6IeC6KiT57e0",
+        "https://www.youtube.com/watch?v=9PPrDeRSLn8&pp=ygUM5omL6IeC6KiT57e0",
+        "https://www.youtube.com/watch?v=fJEJ9EHVN1Q&pp=ygUM5omL6IeC6KiT57e0"
+    ]
+    return links
+#背部
+def back():
+    links = [
+        "https://www.youtube.com/watch?v=pxzsXLjKihQ&pp=ygUM6IOM6YOo6KiT57e0",
+        "https://www.youtube.com/watch?v=47RlYiVVuio&pp=ygUM6IOM6YOo6KiT57e0",
+        "https://www.youtube.com/watch?v=zkiVFffsqyw&pp=ygUM6IOM6YOo6KiT57e0"
+    ]
+    return links
+#核心
+def core():
+    links = [
+        "https://www.youtube.com/watch?v=xYFUWfJf9hs&pp=ygUM5qC45b-D6KiT57e0",
+        "https://www.youtube.com/watch?v=Vb287LMSwlw&pp=ygUG5qC45b-D"
+    ]
+    return links
+#腹部
+def abdomen():
+    links = [
+        "https://www.youtube.com/watch?v=hL5W_WeLisg&pp=ygUM6IW56YOo6KiT57e0",
+        "https://www.youtube.com/watch?v=Rxuw310tkx4&pp=ygUM6IW56YOo6KiT57e0",
+        "https://www.youtube.com/watch?v=L1BTJsDAWjo&pp=ygUM6IW56YOo6KiT57e0",
+        "https://www.youtube.com/watch?v=ymz1bd_f9tc&pp=ygUM6IW56YOo6KiT57e0"
+    ]
+    return links
+#腰部
+def waist():
+    links = [
+        "https://www.youtube.com/watch?v=hfPy1AfYHFU&pp=ygUM6IWw6YOo6KiT57e0",
+        "https://www.youtube.com/watch?v=K1Sd99kwCaY&pp=ygUM6IWw6YOo6KiT57e0"
+    ]
+    return links
+#下肢
+def Lower_limbs():
+    links = [
+        "https://www.youtube.com/watch?v=NinrX2xbezI&pp=ygUM5aSn6IW_6KiT57e0",
+        "https://www.youtube.com/watch?v=z0zQCw4TT4s&pp=ygUM5LiL6IKi6KiT57e0"
+    ]
+    return links
+#大腿
+def thigh():
+    links = [
+        "https://www.youtube.com/watch?v=y0ws24YZoA0&list=PLIWcdCksuONqsO9PBCHEZ-91AZFygLijL",
+        "https://www.youtube.com/watch?v=tmcB-WrsVMw&pp=ygUM5aSn6IW_6KiT57e0"
+    ]
+    return links
+#小腿
+def calf():
+    links = [
+        "https://www.youtube.com/watch?v=YLiq42d4D0Y&pp=ygUM5bCP6IW_6KiT57e0",
+        "https://www.youtube.com/watch?v=G86kueE5de4&t=686s&pp=ygUM5bCP6IW_57e057-S"
+    ]
+    return links
+#慢跑
+def jogging():
+    links = [
+        "https://www.youtube.com/watch?v=7Vm70jRsHn0&pp=ygUM5oWi6LeR6KiT57e0",
+        "https://www.youtube.com/watch?v=uOWWhBISm5Y&pp=ygUS5oWi6LeR5pyJ5rCn6KiT57e0",
+        "https://www.youtube.com/watch?v=ivLyi3nnuu4&pp=ygUM5oWi6LeR6KiT57e0"
+    ]
+    return links
+#游泳
+def swim():
+    links = [
+        "https://www.youtube.com/watch?v=95sdM0GUE40&pp=ygUS5rC05Lit5pyJ5rCn6KiT57e0"
+    ]
+    return links
+#自行車
+def bike():
+    links = [
+        "https://www.youtube.com/watch?v=4kBz0awTqSI&pp=ygUP6Ieq6KGM6LuK6KiT57e0",
+        "https://www.youtube.com/watch?v=_HgaNxu25Q8&pp=ygUP6Ieq6KGM6LuK6KiT57e0",
+        "https://www.youtube.com/watch?v=0CYUoXyLgVo&pp=ygUP6Ieq6KGM6LuK6KiT57e0"
+    ]
+    return links
+#熱身
+def warm_up():
+    links = [
+        "https://www.youtube.com/watch?v=-r0TYqz7INw&pp=ygUM54ax6Lqr6KiT57e0",
+        "https://www.youtube.com/watch?v=h8OfHBzFtdI&pp=ygUM54ax6Lqr6KiT57e0",
+        "https://www.youtube.com/watch?v=Zf_qjFgUg90&pp=ygUM54ax6Lqr6KiT57e0"
+    ]
+    return links
+#收操
+def Take_care():
+    links = [
+        "https://www.youtube.com/watch?v=9_RUKL4QjWM&pp=ygUG5pS25pON",
+        "https://www.youtube.com/watch?v=4SBBu-JYXmw&pp=ygUG5pS25pON",
+        "https://www.youtube.com/watch?v=FHlDmnitOA8&pp=ygUG5pS25pON"
+    ]
+    return links
